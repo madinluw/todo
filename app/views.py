4dd59c6d2bed7dd97b52283a6cfbb454f1071907
user <user@users-MacBook-Pro.local>
2023-03-16 20:24:11 -0700

    first init

---
+++ app/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from .models import Todo
+from django.http import HttpResponseRedirect
+# Create your views here.
+
+def main(request):
+    todo = Todo.objects.all
+    context = {'todo': todo}
+    return render(request, 'index.html', context)
+
+def create_todo(request):
+    new_todo = Todo()
+    if request.method == 'POST':
+        new_todo.title = request.POST.get('title')
+        new_todo.description = request.POST.get('description')
+        new_todo.save()
+        return HttpResponseRedirect('/')
+    return HttpResponseRedirect('/')
+
+def delete(request, id):
+    todo = Todo.objects.get(id=id)
+    todo.delete()
+    return HttpResponseRedirect('/')
+
+def update(request, id):
+    todo = Todo.objects.get(id=id)
+    if request.method == 'POST':
+        todo.title = request.POST.get('title')
+        todo.description = request.POST.get('description')
+        todo.save()
+        return HttpResponseRedirect('/')
+    return render(request, 'update.html', {'todo':todo})
